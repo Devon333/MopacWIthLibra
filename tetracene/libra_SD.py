fc39707c602f87e257eef2aeedde7e9b3fed4504
Devon333 <devondillon333@gmail.com>
2023-07-10 18:10:28 -0400

    Update libra_SD.py
    added doc strings, remove some unused code.

--- tetracene/libra_SD.py
+++ tetracene/libra_SD.py
@@ -51,8 +51,10 @@ clrs_index = ["11", "21", "31", "41", "12", "22", "32", "13","23", "14", "24"]
 # setting number of files used to calculate NACs
 start = 0
 final = 1000
+
 # setting time step
 dt = 1*units.fs2au
+
 # setting up params dict that is use by libra functions to read matrices
 params = {}
 if os.path.exists('*.png'):
@@ -74,13 +76,11 @@ params["number_of_states"]    = 3
 
 
 
-#sd_basis =read_mopac_num_of_SD_states(params)
 
-
-# reading basis from mopac output file
+# function that reads basis from mopac output files
 sd_basis=read_mopac_SD_config(params)
 
-# reading matrices from h2oTraj/
+# reading matrices from tetraceneTraj/
 params.update( {  "data_dim":6, "isnap":start,"fsnap":final,"active_space":range(0,6),"get_imag":0,"get_real":1,
                   "data_re_prefix": "tetraceneTraj/tetracene_S_", "data_re_suffix": "_re.txt",
                   "data_im_prefix": "tetraceneTraj/tetracene_S_", "data_im_suffix": "_im.txt"
@@ -102,18 +102,6 @@ params.update( {  "data_dim":10,'active_space':range(0,10),"isnap":start,"fsnap"
 Esd = data_read.get_data(params)
 print("GOT SD_midpoints")
 Esd[0].show_matrix()
-#params.update( {  "data_dim":10, "isnap":start,"fsnap":final,"active_space":range(0,10),"get_imag":0,"get_real":1,
-#                  "data_re_prefix": "tetraceneTraj/tetracene_T_", "data_re_suffix": "_re.txt",
-#                  #"data_im_prefix": "Tetracene_indo_traj/h2_T_", "data_im_suffix": "_im.txt"
-#               })
-#T = data_read.get_data(params)
-#print("GOT CI_matrix")
-#params.update( {  "data_dim":10, "isnap":start,"fsnap":final,
-#                  "data_re_prefix": "tetraceneTraj/tetracene_CI_mid_E_", "data_re_suffix": "_re.txt",
-#                  "data_im_prefix": "tetraceneTraj/tetracene_CI_mid_E_", "data_im_suffix": "_im.txt"
-#               })
-#E = data_read.get_data(params)
-#print("GOT CI_midpoints")
 
 raw_basis=get_raw_basis(params)
 basis= reindex_basis(raw_basis)
@@ -121,40 +109,28 @@ basis= reindex_basis(raw_basis)
 
 
 #function that calculates overlap between Slater determinants using libra functions
-def SD_ovlp(basis, time_ov_mat):#,phases):
+def SD_ovlp(basis, time_ov_mat):
+  """basis -> list[list]
+     time_ov_mat -> list[CMATRIX]
+     takes in list of SD basis lists (list with indexes of KS orbitals used to describe 
+     a single SD) and computes overlaps between SD bases by pulling the appropriate indices
+     from the time doverlap matrix (time_ov_mat)
+     returns a matrix with overlaps of SD overlaps """
   N, M = len(basis), len(basis)
   res = CMATRIX(N,M)
   for i in range(len(basis)):
       for j in range(len(basis)):
          test = mapping.ovlp_arb(basis[i], basis[j], time_ov_mat,False)
-         #test *= phases.get(j,0).real
          res.set(i,j,test)
          #print(test) 
   return res
 
-#function that populates matrices with Slater determinant energies using libra functions
-def SD_energy(basis,E_mat):
-  N, M = len(basis), len(basis)
-  res = CMATRIX(N,M)
-  for i in range(len(basis)):
-     test = mapping.energy_arb(basis[i],E_mat)
-     res.set(i,i,test)
-     #print(test)
-  #res.show_matrix() 
-  return res
-
-#function that populates matrix with CI energies energy usig libra functions
-def CI_energy(basis,E_mat):
-  N, M = len(basis), len(basis)
-  res = CMATRIX(N,M)
-  for i in range(len(basis)):
-     test = mapping.energy_arb(basis[i],E_mat)
-     res.set(i,i,test)
-     #print(test)
-  #res.show_matrix() 
-  return res
 
 def Mat_avg(cmatList):
+    """cmatList -> list[CMATRIX]
+    takes in a list of cmatrices and returns the average value of each matrix element 
+    over all of the in a numpy matrix
+    """
     cols = cmatList[0].num_of_cols
     matAvg = np.zeros((cols,cols))
     steps = len(cmatList)
@@ -179,11 +155,11 @@ ks_phases=step3.apply_phase_correction_general(tim_ov)
 Stsd=[]
 Ssd=[]
 
-#building lists of overlap and time overlap matrices using function built above
+#building lists of overlap and time overlap matrices using SD_ovlp
 for time in range(final-start):
-    Stsd.append(SD_ovlp(basis, tim_ov[time]))#,ks_phases[time]))
-    Ssd.append(SD_ovlp(basis, S[time]))#,ks_phases[time]))
-
+    Stsd.append(SD_ovlp(basis, tim_ov[time]))
+    Ssd.append(SD_ovlp(basis, S[time]))
+    
 print("this is Stsd ", Stsd[0].show_matrix() )    
 print("this is Ssd ", Ssd[0].show_matrix() )    
 
@@ -192,67 +168,9 @@ sd_phases=step3.apply_phase_correction_general( Stsd )
 #print("the phases",sd_phases[0].get(1,0).real)
 
 
-#T[0].show_matrix()
-#SD2CI=[]
-##SD2CI2=[]
-##T_co=[]
+
 CIs=len(basis)
 print(f"length of CI basis {CIs}")
-#exit()
-#
-#
-## Building Transformation/CI matrix SD2CI
-#for time in range(final-start):
-#    SD2CI.append(CMATRIX(CIs,CIs))
-#    for row in range(CIs):
-#        for col in range(CIs):
-#            val= (T[time].get(row,col))#*(1.0+0.0j)
-#            print(f"val before phase corr {val}")
-#            val *= sd_phases[time].get(row,0).real
-#            SD2CI[time].set(row,col,val)
-#            print(f'this is a value put in the SD2CI matrix {val}')
-#    
-#
-## normalizing rows of Transformation matrix
-#    for row in range(CIs):
-#        norm=0.0
-#        for col in range(CIs):
-#            norm += abs(SD2CI[time].get(row,col))**2
-#        norm = 1.0/math.sqrt(norm)
-#        SD2CI[time].scale(-1,row,norm*(1.0+0.0j))
-#for time in range(final-start):
-#    for row in range(CIs):
-#      for col in range(CIs):
-#          val = SD2CI[time].get(row,col)
-#          SD2CI[time].set(row,col,val)
-
-
-#for I in range(len(SD2CI)-1):
-#  print(f'Stsd[{I}] {Stsd[I].real().show_matrix()}')
-#  print(f'sd_phases[{I}]{sd_phases[I].real().show_matrix()}')
-#  print(f'SD2CI[{I}].H() {SD2CI[I].H().real().show_matrix()}')
-#  print(f'SD2CI[{I+1}] {SD2CI[I].real().show_matrix()}')
-#  print(f'Stsd[{I+1}] {Stsd[I+1].real().show_matrix()}')
-#  print(f'sd_phases[{I+1}]{sd_phases[I+1].real().show_matrix()}')
-##  print(f'SD2CI[{I+1}].H() {SD2CI[I+1].H().real().show_matrix()}')
-##  print(f'SD2CI[{I+2}] {SD2CI[I+2].real().show_matrix()}')
-#
-##exit()
-##calculating CI overlap and time overlaps
-#for time in range(len(Ssd)-1):
-#    Stci.append( SD2CI[time].H() * Stsd[time] * SD2CI[time+1])    
-#print(f'Stci[0] {Stci[0].real().show_matrix()}')
-#
-#
-#
-#for time in range(len(Ssd)):
-#    Sci.append( SD2CI[time].H() * Ssd[time] * SD2CI[time])    
-#
-#step3.apply_orthonormalization_general( Sci, Stci )
-#ci_phases=step3.apply_phase_correction_general( Stci )
-#print("phase correction[0]",ci_phases[0].show_matrix())
-#print("phase correction[1]",ci_phases[1].show_matrix())
-
 
 
 dt = 1*units.fs2au
@@ -314,31 +232,12 @@ md_time   = np.array( md_time )
 sd_res= Mat_avg(Hvib)
 sd_tNACs = []
 
-#for i in range(nCIs):
-#    ci_tNACs.append( [] )
-#    sd_tNACs.append( [] )
-#    for j in range(nCIs):
-#        #ci_tNACs[i].append( ci_res.get(i,j).imag * 1000.0 / units.ev2Ha )        
-#        sd_tNACs[i].append( sd_res.get(i,j).imag * 1000.0 /units.ev2Ha  )        
-#        ci_tNACs[i].append( ci_res.get(i,j).imag * 1000.0 /units.ev2Ha  )        
-#sd_tNACs = np.array(sd_tNACs)
-#ci_tNACs = np.array(ci_tNACs)
-#
-#
+
 nstates = 10
 orbitals = ["GS","42 -> 43","42 -> 44","42 -> 45","41 -> 43","41 -> 44","41 -> 45","40 -> 43","40 -> 44","40 -> 45"]
 state_label = ["GS","2","3","4","5","6","7","8","9","10"]#['H-2','H-1','H','L','L+1','L+2'] 
 state_label_x = ["GS","H->L","H->L+1","H->L+2","H-1->L","H-1->L+1","H-1->L+2","H-2->L","H-2->L+1","H-2->L+2"]
-## Figures - Time-Dependent CI Data
-#plt.figure(num=None, figsize=(6,6), dpi=100, edgecolor='black', frameon=True)
-#plt.title('INDO CI Energies', fontsize=25)
-#plt.xlabel('Time, fs')
-#plt.xticks(fontsize=15)
-#plt.yticks(fontsize=15)
-#for state in range(1,nstates ):    
-#    plt.plot(md_time, CI_energy[state], label=state_label[state], linewidth=2)
-#plt.tight_layout()
-#plt.savefig("CI_energytime.png")
+
 
 fig=plt.figure(num=None, figsize=(6,6), dpi=100, edgecolor='black', frameon=True)
 plt.title('INDO SD NACs', fontsize=25)
